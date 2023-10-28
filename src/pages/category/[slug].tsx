22f153c9b6c24c47db9234ea05e9c3f0d08ed942
maestroharyor <ayomide.odewale1@gmail.com>
2023-10-28 02:21:53 +0100

    Error handling

--- src/pages/category/[slug].tsx
+++ src/pages/category/[slug].tsx
@@ -119,7 +119,7 @@ const CategoryPage = ({
   return (
     <DefaultLayout
       title={`${category?.name || "Category"} Posts`}
-      metadescription={category.description}
+      metadescription={category?.description || ""}
     >
       <div className="py-10 space-y-8">
         <h1 className="text-3xl font-bold text-center lg:text-5xl">
@@ -130,9 +130,10 @@ const CategoryPage = ({
           Category
         </h1>
         <div className="grid grid-cols-1 md:grid-cols-2 gap-y-16 gap-x-10 py-10">
-          {posts.map((post: postDatatype) => (
-            <Postcard key={post.id} post={post} />
-          ))}
+          {posts ||
+            [].map((post: postDatatype) => (
+              <Postcard key={post.id} post={post} />
+            ))}
         </div>
         <PostPagination />
       </div>
